46dc9bceeefb9c389f02fba15e2f4079a2eb80e2
Anand Otiv <w0858857@myscc.ca>
2025-03-28 23:21:58 -0400

    Initial Commit

---
+++ TelevisionShowApplication/TelevisionShowApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  TelevisionShowApplicationApp.swift
+//  TelevisionShowApplication
+//
+//  Created by Anand Otiv on 2025-03-28.
+//
+
+import SwiftUI
+
+@main
+struct TelevisionShowApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
