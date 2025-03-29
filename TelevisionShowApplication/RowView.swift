c79cedbba13aaf2865bdc167c04756a88d71f590
Anand Otiv <w0858857@myscc.ca>
2025-03-29 00:06:45 -0400

    Made a rought draft of Detail View

---
+++ TelevisionShowApplication/RowView.swift
@@ -0,0 +1,18 @@
+//
+//  RowView.swift
+//  TelevisionShowApplication
+//
+//  Created by Anand Otiv on 2025-03-28.
+//
+
+import SwiftUI
+
+struct RowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RowView()
+}
---
+++ TelevisionShowApplication/TVShow.swift
@@ -0,0 +1,8 @@
+//
+//  TVShow.swift
+//  TelevisionShowApplication
+//
+//  Created by Anand Otiv on 2025-03-28.
+//
+
+import Foundation
