c77aabf1b0cca26d874eff854543aade03e2f851
Anand Otiv <w0858857@myscc.ca>
2025-03-30 02:31:44 -0400

    Fixed the details view and row view Added Entry with tabs to the main view and the content view

---
+++ TelevisionShowApplication/EntryView.swift
@@ -0,0 +1,37 @@
+//
+//  WebView.swift
+//  TelevisionShowApplication
+//
+//  Created by Anand Otiv on 2025-03-29.
+//
+
+
+//
+//  WebView.swift
+//  CampingApp
+//
+//  Created by Christopher Takaki on 2025-03-24.
+//
+
+import WebKit
+import SwiftUI
+
+struct WebView: UIViewRepresentable{
+    let webText: String?
+    func makeUIView(context: Context) -> WKWebView {
+        WKWebView()
+    }
+    
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        guard let text = webText, let url = URL(string: text) else {
+            return
+        }
+        
+        let urlRequest = URLRequest(url: url)
+        uiView.load(urlRequest)
+    }
+    
+    typealias UIViewType = WKWebView
+    
+    
+}
